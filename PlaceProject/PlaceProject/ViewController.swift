db7d6a592e3247fe3fb9f0a9f19c1e51fdac70c5
Sunghyun Kim <sunghyun_k@icloud.com>
2019-09-26 17:49:54 +0900

    프로젝트 생성

---
+++ PlaceProject/PlaceProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlaceProject
+//
+//  Created by 김성현 on 26/09/2019.
+//  Copyright © 2019 Sunghyun Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
